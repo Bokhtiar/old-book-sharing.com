ac1e2ec3f236eb18ab4d6e89ca677d36d0233190
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-06-11 13:44:32 +0600

    single book show

--- resources/views/user/book/book.blade.php
+++ resources/views/user/book/book.blade.php
@@ -20,6 +20,14 @@ a{
 }
 </style>
 <section class="container my-5">
+  <nav aria-label="breadcrumb" style="background-color: #F2F4F4">
+    <ol class="breadcrumb py-3 px-3">
+      <li class="breadcrumb-item"><a style="text-decoration: none"  href="{{url('/')}}">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Book</li>
+    </ol>
+  </nav>
+
+
     <div class="row">
         <div class="col-md-5 col-sm-12 col-lg-5">
             <div class="container polaroid-hover">
@@ -35,49 +43,40 @@ a{
             </div>
 
             <div class="my-5">
-                <span>Category : {{ $book->category->name }}</span><br>
-                <span>Author : {{ $book->author }} </span><br>
+                <span class="">Category : {{ $book->category->name }}</span><br><br>
+                <span class="">Author : {{ $book->author }} </span><br><br>
                 <span>Description: {{ $book->description }}</span>
             </div>
         </div>
     </div>
 </section>
 
-<style>
-  .polaroid{
-
-  padding: 10px;
-  box-shadow: 2px 2px 5px 2px #eee9e9;
-  height: 350px;
-  text-align: center;
-  }
-  a{
-      text-decoration: none;
-      color:black;
-  }
-  </style> 
 
 <section class="container">
-    <section class="my-5"><!--books-->
-        <div class=" my-3">
-          <h2>Releted Product's</h2>
-        </div>
-        <div class="container">
-          <div class="row p-2">
-            @foreach ($books as $book)
-            <div class=" col-sm-6 col-6 col-md-3 col-lg-3 polaroid my-4 rounded-lg" >
-                <div class="card" style="width: 100%; margin-left:0px;" >
-                <a href="{{ url('book/detail',$book->id) }}">
-                  <img height="200px" width="100%" src="{{ asset($book->image) }}" class="card-img-top p-2"  alt="...">
-                </a>
-                  <div class="card-body">
-                    <p class="card-text"><a class="text-dark" href="{{ url('book/detail',$book->id) }}">{{ $book->title }}</a>.</p>
-                  </div>
-                </div>
+  <div class="d-flex justify-content-between shadow-sm  px-4 py-2">
+    <h2>All book</h2>
+    <span class="mt-2"><i class="btn btn-sm btn-outline-success p-1 fas fa-list"></i>  <i class="btn btn-sm btn-outline-success p-1 far fa-list-alt"></i></span>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-12 col-md-9 col-lg-9">
+      <div class="row p-2">
+        @foreach ($books as $book)
+        <div class="col-sm-6 col-6 col-md-3 col-lg-3  my-4 rounded-lg" >
+            <div class=" shadow p-3" style="width: 100%; margin-left:0px;" >
+              <a href="{{ url('book/detail',$book->id) }}">
+                  <img height="250px" width="100%" src="{{ asset($book->image) }}" class="card-img-top p-2"  alt="...">
+              </a>
+              <div class="card-body d-flex justify-content-between pt-2">
+                <p class="card-text text-capitalize h6"><a href="{{ url('book/detail',$book->id) }}">{{ $book->title }}</a> </p>
+                <span>{{$book->price}}Tk</span>
               </div>
-            @endforeach
+            </div>
           </div>
-        </div>
-      </section><!-- end of book-->
+        @endforeach
+      </div>
+    </div>
+  </div>
 </section>
+
 @endsection
